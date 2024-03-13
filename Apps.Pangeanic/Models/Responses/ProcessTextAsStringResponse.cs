aaea9d38ab6333b12776300289038a9629363f19
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-03-13 11:26:04 +0200

    Updated Process text action

---
+++ Apps.Pangeanic/Models/Responses/ProcessTextAsStringResponse.cs
@@ -0,0 +1,12 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Pangeanic.Models.Responses;
+
+public class ProcessTextAsStringResponse
+{
+    [Display("Source text")]
+    public string Source { get; set; }
+    
+    [Display("Target text")]
+    public string Target { get; set; }
+}
