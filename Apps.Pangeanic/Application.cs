a1c76ef5c509876befde8b29653c19734f4103be
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:51:38 +0300

    Categories (#4)
    * added categories
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.Pangeanic/Application.cs
+++ Apps.Pangeanic/Application.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Pangeanic;
 
-public class Application : IApplication
+public class Application : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe];
+        set { }
+    }
+    
     public string Name
     {
         get => "App";
